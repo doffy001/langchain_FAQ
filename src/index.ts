a8a49b9dff0f620c847ad2aeed6f95c4cd1f5176
Doffy <doffy.fastcodingvn@gmail.com>
2024-06-04 19:29:20 +0700

    [fix] openAI model

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-let str: string = 'hello world!';
-console.log(str);
+import { OpenAI } from "@langchain/openai";
+require('dotenv').config();
+
+export const run = async () => {
+  const question = 'What would be a good company name a company that makes colorful socks?';
+  const model = new OpenAI({ temperature: 0.9 });
+  const result = await model.invoke(question);
+  console.log('🚀 ~ res:', result);
+};
+
+run();
